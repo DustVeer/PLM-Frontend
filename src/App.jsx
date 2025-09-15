5b069f1a1a8ee534460ad8185699e7ca3d061416
DustVeer <dveerdonk@gmail.com>
2025-09-15 09:19:01 +0200

    Started over. Because blindly following Chat isn't a way of learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
-import RequireAuth from "./auth/RequireAuth";
-import LoginPage from "./pages/LoginPage";
-import StockPage from "./pages/StockPage";
-import FlowPage from "./pages/StockPage";
-
-const router = createBrowserRouter([
-  { path: "/login", element: <LoginPage /> },
-  {
-    element: <RequireAuth />, // alles hieronder is achter login
-    children: [
-      { path: "/", element: <StockPage /> },
-      { path: "/flowpage", element: <FlowPage /> },
-      // meer private routes…
-    ],
-  },
-]);
 
 function App() {
-  return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
+  return <AuthProvider></AuthProvider>;
 }
 
 export default App;
--- src/api's/products.js
+++
@@ -1,7 +0,0 @@
-import { api } from "./client";
-
-export const ProductsApi = {
-  getById: (id) => api.get(`/products/${id}`),
-  list: () => api.get(`/products`),
-  create: (dto) => api.post(`/products`, dto),
-};
