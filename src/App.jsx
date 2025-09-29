68e0be51d722a08f13ebbda68c3af4b658624327
DustVeer <dveerdonk@gmail.com>
2025-09-29 18:56:49 +0200

    Added a Dashboard and a Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,28 @@
 import React, { use } from "react";
 import { useState, useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AuthProvider } from "./auth/AuthContext";
-import ProductsApi from "./api's/products";
-import RequireAuth from "./auth/RequireAuth";
-import LoginPage from "./pages/LoginPage";
-import StockPage from "./pages/StockPage";
-import FlowPage from "./pages/StockPage";
+import Header from "./components/Header";
+import Sidebar from "./components/SideBar";
+import { SidebarProvider } from "./context/SidebarContext";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
-// const router = createBrowserRouter([
-//   { path: "/login", element: <LoginPage /> },
-//   {
-//     element: <RequireAuth />, // alles hieronder is achter login
-//     children: [
-//       { path: "/", element: <StockPage /> },
-//       { path: "/flowpage", element: <FlowPage /> },
-//       // meer private routes…
-//     ],
-//   },
-// ]);
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const requestData = async () => {
-      try{
-        const response = await ProductsApi.list();
-        setProducts(JSON.stringify(response));
-      }
-      catch(error){
-        console.error('Error fetching products:', error);
-      }
-      };
-    requestData();
-
-    }
-    , [])
+  
 
   return (
-    <AuthProvider>
-      <p>{products}</p>
-    </AuthProvider>
+    <>
+      <Login />
+      {/* <SidebarProvider>
+        <div className="min-h-screen overflow-x-hiddenoverlay fixed inset-0z-40 hidden opacity-0 transition-opacity duration-300"></div>
+        <Header />
+        <div className="pt-16 max-w-7xl mx-auto flex">
+          <Sidebar />
+          <Dashboard />
+        </div>  
+      </SidebarProvider> */}
+    </>
+
   );
 }
 
