f6b381dfb4b56c3c05161a0023ce498cf34df1ab
DustVeer <dveerdonk@gmail.com>
2025-12-08 19:59:02 +0100

    Removed base /PLM-Frontend/
    This base was allready removed on the branch RaspberryPiWebserver. And these 2 where merged. So I don't know what went worng.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: '/PLM-Frontend/',
   plugins: [tailwindcss(), react()],
   server: {
     port: 5173,
